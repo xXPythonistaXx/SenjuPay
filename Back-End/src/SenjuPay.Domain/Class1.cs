f7f6308b94d5591cddc3e9f47610d9ea280bca66
xXPythonistaXx <gbral.js@gmail.com>
2022-03-13 12:52:47 -0300

    9 - Dividindo as camadas de dominio.

---
+++ Back-End/src/SenjuPay.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SenjuPay.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Back-End/src/SenjuPay.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SenjuPay.Persistence
+{
+    public class Class1
+    {
+    }
+}
